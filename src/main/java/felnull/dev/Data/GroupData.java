b4e9b70cebbdca4c3bcdd2309657c88aed244564
miyabi0333 <57523862+Arisa9006@users.noreply.github.com>
2024-11-15 22:03:19 +0900

    ミス修正 V1.0.0

--- src/main/java/felnull/dev/Data/GroupData.java
+++ src/main/java/felnull/dev/Data/GroupData.java
@@ -6,9 +6,9 @@ import java.util.Map;
 import java.util.Set;
 
 public class GroupData {
-    public final String groupName;
-    public Set<Player> playerList;
-    public Map<Player,String[]> playerPermission;
+    public final String groupName; //グループ名　プレイヤー個人の場合はプレイヤーUUID
+    public Set<Player> playerList; //グループ所属のプレイヤーリスト 最低１つは格納されるはず
+    public Map<Player,String[]> playerPermission; //プレイヤーが保持している役職 //StringじゃなくてENUMでやるべきかもです
     public GroupData (String groupName, Set<Player> playerList, Map<Player,String[]> playerPermission) {
         this.groupName = groupName;
         this.playerList = playerList;
--- src/main/java/felnull/dev/Data/InventoryData.java
+++ src/main/java/felnull/dev/Data/InventoryData.java
@@ -1,21 +1,22 @@
 package felnull.dev.Data;
 
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.Map;
 import java.util.Set;
 
 public class InventoryData {
-    public final String[] userTag;
-    public final String displayName;
-    public final int rows;
-    public final Set<String> requirePermission;
-    public Map<Integer, Inventory> inventorySlot;
-    public InventoryData(String[] userTag, String displayName, int rows, Set<String> requirePermission, Map<Integer, Inventory> inventorySlot) {
+    public final String[] userTag; //ユーザーがインベントリに対してつけるタグ
+    public final String displayName; //表示名
+    public final int rows; //行数
+    public final Set<String> requirePermission; //インベントリを開くのに必要な最低限の権限
+    public Map<Integer, ItemStack> itemStackSlot; //スロットに対応するアイテム
+    public InventoryData(String[] userTag, String displayName, int rows, Set<String> requirePermission, Map<Integer, ItemStack> itemStackSlot) {
         this.userTag = userTag;
         this.displayName = displayName;
         this.rows = rows;
         this.requirePermission = requirePermission;
-        this.inventorySlot = inventorySlot;
+        this.itemStackSlot = itemStackSlot;
     }
 }
