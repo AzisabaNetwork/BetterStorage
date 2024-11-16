1728226b9c1ac250df336d15edd11d66e18ab803
miyabi0333 <57523862+Arisa9006@users.noreply.github.com>
2024-11-17 00:51:54 +0900

    変なミス修正 (Stringにすべきところがintだった)

--- src/main/java/dev/felnull/Data/StorageData.java
+++ src/main/java/dev/felnull/Data/StorageData.java
@@ -6,13 +6,13 @@ import java.util.Map;
 import java.util.Set;
 
 public class StorageData {
-    public final int groupName;//グループ名(個人プレイヤーもグループ扱い)
+    public final String groupName;//グループ名(個人プレイヤーもグループ扱い)
     public final boolean personal;//個人用かグループ用かのフラグ
     public final Set<String> requireBankPermission;//ストレージ直下の金庫の要求パーミッション(BukkitPermではない)
     public int bankMoney;//ストレージ付属金庫の値
     public Map<String,InventoryData> storageInventry;//ストレージに含まれているインベントリデータ　キーのStringはページ名
 
-    public StorageData(int groupName, boolean personal, Set<String> requireBankPermission, Map<String,InventoryData> storageInventry, int bankMoney) {
+    public StorageData(String groupName, boolean personal, Set<String> requireBankPermission, Map<String,InventoryData> storageInventry, int bankMoney) {
         this.groupName = groupName;
         this.personal = personal;
         this.requireBankPermission = requireBankPermission;
