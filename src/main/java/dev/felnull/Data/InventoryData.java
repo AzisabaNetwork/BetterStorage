c1b2ab5bc4d40e09dd0491abd013989505f9b4ad
Arisa9006 <57523862+Arisa9006@users.noreply.github.com>
2024-11-20 11:00:40 +0900

    無駄なfinalを削除

--- src/main/java/dev/felnull/Data/InventoryData.java
+++ src/main/java/dev/felnull/Data/InventoryData.java
@@ -8,10 +8,10 @@ import java.util.Map;
 import java.util.Set;
 
 public class InventoryData {
-    public final String[] userTag; //ユーザーがインベントリに対してつけるタグ
-    public final String displayName; //表示名
+    public String[] userTag; //ユーザーがインベントリに対してつけるタグ
+    public String displayName; //表示名
     public final int rows; //行数
-    public final Set<String> requirePermission; //インベントリを開くのに必要な最低限の権限
+    public Set<String> requirePermission; //インベントリを開くのに必要な最低限の権限
     public Map<Integer, ItemStack> itemStackSlot; //スロットに対応するアイテム
     public InventoryData(String[] userTag, String displayName, int rows, Set<String> requirePermission, Map<Integer, ItemStack> itemStackSlot) {
         this.userTag = userTag;
