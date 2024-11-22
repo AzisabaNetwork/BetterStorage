3a886d9f888275aed40e19b517beeb5e4d5c95fe
Arisa9006 <57523862+Arisa9006@users.noreply.github.com>
2024-11-22 11:06:02 +0900

    Update GroupData.java

--- src/main/java/dev/felnull/Data/GroupData.java
+++ src/main/java/dev/felnull/Data/GroupData.java
@@ -8,7 +8,7 @@ import java.util.Set;
 public class GroupData {
     public final String groupName; //グループ名　プレイヤー個人の場合はプレイヤーUUID
     public Set<Player> playerList; //グループ所属のプレイヤーリスト 最低１つは格納されるはず
-    public Map<Player,String[]> playerPermission; //プレイヤーが保持している役職 //StringじゃなくてENUMでやるべきかもです
+    public Map<Player,String[]> playerPermission; //プレイヤーが保持している役職
     public GroupData (String groupName, Set<Player> playerList, Map<Player,String[]> playerPermission) {
         this.groupName = groupName;
         this.playerList = playerList;
