1bdfe0599c025be8ccdfd1aedc80c3a7b65fd786
Arisa9006 <57523862+Arisa9006@users.noreply.github.com>
2024-11-25 13:31:46 +0900

    ストレージ構造を変更RootをGroupDataに

--- src/main/java/dev/felnull/Data/GroupData.java
+++ src/main/java/dev/felnull/Data/GroupData.java
@@ -9,10 +9,17 @@ public class GroupData {
     public final String groupName; //グループ名　プレイヤー個人の場合はプレイヤーUUID
     public Set<Player> playerList; //グループ所属のプレイヤーリスト 最低１つは格納されるはず
     public Map<Player,String[]> playerPermission; //プレイヤーが保持している役職
-    public GroupData (String groupName, Set<Player> playerList, Map<Player,String[]> playerPermission) {
+    public boolean isPrivate; //個人用グループか否か
+    public StorageData storageData; //グループ保有のストレージデータ null許容
+
+    public GroupData (String groupName, Set<Player> playerList, Map<Player,String[]> playerPermission, boolean isPrivate, StorageData storageData) {
         this.groupName = groupName;
         this.playerList = playerList;
         this.playerPermission = playerPermission;
+        this.isPrivate = isPrivate;
+        storageData.groupName = groupName;
+        storageData.groupData = this;
+        this.storageData = storageData;
     }
 
 }
--- src/main/java/dev/felnull/Data/StorageData.java
+++ src/main/java/dev/felnull/Data/StorageData.java
@@ -6,15 +6,13 @@ import java.util.Map;
 import java.util.Set;
 
 public class StorageData {
-    public final String groupName;//グループ名(個人プレイヤーもグループ扱い)
-    public final boolean personal;//個人用かグループ用かのフラグ
+    public String groupName;//グループ名(個人プレイヤーもグループ扱い)
+    public GroupData groupData;//ストレージの所属グループ
     public final Set<String> requireBankPermission;//ストレージ直下の金庫の要求パーミッション(BukkitPermではない)
     public int bankMoney;//ストレージ付属金庫の値
     public Map<String,InventoryData> storageInventry;//ストレージに含まれているインベントリデータ　キーのStringはページ名
 
-    public StorageData(String groupName, boolean personal, Set<String> requireBankPermission, Map<String,InventoryData> storageInventry, int bankMoney) {
-        this.groupName = groupName;
-        this.personal = personal;
+    public StorageData(Set<String> requireBankPermission, Map<String,InventoryData> storageInventry, int bankMoney) {
         this.requireBankPermission = requireBankPermission;
         this.storageInventry = storageInventry;
         this.bankMoney = bankMoney;
